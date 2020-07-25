8b232e3f8ef0311507d8871e9f8f0dc58b17011d
MamzelNyf <fanny@shedesign.dev>
2020-07-25 12:07:21 +0800

    add choices

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime 
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,6 +10,13 @@ class Question(models.Model):
     #name of the fields
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    def __str__(self):
+        # add __str__() methods to your models to create objects’ representation
+        # add a method to be return the object question
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(
@@ -16,4 +26,6 @@ class Choice(models.Model):
         #delete the choices if the question if deleted, CASCADE function
     )
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0) 
+    def __str__(self):
+            return self.choice_text
